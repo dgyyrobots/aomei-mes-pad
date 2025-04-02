0afa95dfd62e7c360a0630e397ff7df33582ef92
zhangjinjian <1016417093@qq.com>
2025-04-02 15:29:08 +0800

    fix:  git 冲突

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import curdGenerator from './plop-template/curd/prompt.mjs'
 
 export default (plop) => {
   plop.setGenerator('curd', curdGenerator)
 }
-=======
-import curdGenerator from './plop-template/curd/prompt.mjs'
-
-export default (plop) => {
-  plop.setGenerator('curd', curdGenerator)
-}
->>>>>>> c9b2a8adc967d14a9f2577414c8fcccde1704016
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default {
   extends: ['stylelint-config-recommended-scss', 'stylelint-config-recommended-vue', 'stylelint-config-recess-order'],
   rules: {
@@ -12,18 +11,3 @@ export default {
   },
   ignoreFiles: ['dist/**/*', 'index.html'],
 }
-=======
-export default {
-  extends: ['stylelint-config-recommended-scss', 'stylelint-config-recommended-vue', 'stylelint-config-recess-order'],
-  rules: {
-    'at-rule-no-unknown': null,
-    'property-no-unknown': null,
-    'function-no-unknown': null,
-    'selector-class-pattern': null,
-    'no-descending-specificity': null,
-    'scss/no-global-function-names': null,
-    'selector-pseudo-class-no-unknown': null,
-  },
-  ignoreFiles: ['dist/**/*', 'index.html'],
-}
->>>>>>> c9b2a8adc967d14a9f2577414c8fcccde1704016
